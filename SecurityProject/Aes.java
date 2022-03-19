abe6893f0c534436ff23ba9350b7680d0b36b2df
ernes <ernest.li@ontariotechu.net>
2022-03-18 20:58:31 -0400

    Added password string to SecretKey object with Password-Based Key Derivation Function 2(PBKDF2). Added image encryption and decryption functions in AES class but is not implemented to the socket client/server yet. Added encrypted images in meme2 folder, and one decrypted image in cat2 folder

--- SecurityProject/Aes.java
+++ SecurityProject/Aes.java
@@ -1,18 +1,41 @@
 package SecurityProject;
 
-import java.io.UnsupportedEncodingException;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.nio.ByteBuffer;
+import java.security.InvalidKeyException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
 import java.util.Arrays;
 import java.util.Base64;
 
-import javax.crypto.Cipher;
+import javax.crypto.*;
+import javax.crypto.spec.PBEKeySpec;
 import javax.crypto.spec.SecretKeySpec;
+import javax.imageio.ImageIO;
 
 public class Aes {
-    private static SecretKeySpec secretKey;
+    private static SecretKey secretKey;
     private static byte[] key;
+    public static void main(String[] argv) throws InvalidKeySpecException, NoSuchAlgorithmException, IllegalBlockSizeException, NoSuchPaddingException, IOException, BadPaddingException, InvalidKeyException {
+        //this void function is for demotration purposes and can be deleted
+        String keyPassword = "crypto";
+        String salt = "sdfksdflk";
+        System.out.println("key password: " + keyPassword + " salt: "+salt);
+        System.out.println("Get Key for AES using Password-Based Key Derivation Function(PBKDF2)");
+        secretKey = getKeyFromPassword(keyPassword,salt);
+        decryptFile(secretKey,new File("SecurityProject/Cats/meme2/anger.jpeg"),new File("SecurityProject/Cats/cute2/anger.jpeg"));
+        System.out.println("encrypting");
+        String encrypt = encrypt("HelloWorld",keyPassword);
 
+        System.out.println(encrypt);
+        System.out.println("decrypting");
+        String decrypt = decrypt(encrypt, keyPassword);
+        System.out.println(decrypt);
+
+    }
     public static void setKey(final String myKey) {
         MessageDigest sha = null;
         try {
@@ -25,10 +48,18 @@ public class Aes {
             e.printStackTrace();
         }
     }
+    public static SecretKey getKeyFromPassword(String password, String salt) throws InvalidKeySpecException, NoSuchAlgorithmException {
+
+        SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+        KeySpec spec = new PBEKeySpec(password.toCharArray(), salt.getBytes(), 65536, 256);
+        SecretKey secret = new SecretKeySpec(factory.generateSecret(spec)
+                .getEncoded(), "AES");
+        return secret;
+    }
 
     public static String encrypt(final String strToEncrypt, final String secret) {
         try {
-            setKey(secret);
+            //setKey(secret);
             Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
             cipher.init(Cipher.ENCRYPT_MODE, secretKey);
             return Base64.getEncoder()
@@ -41,7 +72,7 @@ public class Aes {
 
     public static String decrypt(final String strToDecrypt, final String secret) {
         try {
-            setKey(secret);
+            //setKey(secret);
             Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5PADDING");
             cipher.init(Cipher.DECRYPT_MODE, secretKey);
             return new String(cipher.doFinal(Base64.getDecoder()
@@ -51,4 +82,91 @@ public class Aes {
         }
         return null;
     }
+    public static void encryptFile(SecretKey key,File inputFile, File outputFile) throws InvalidKeyException, IOException, IllegalBlockSizeException, BadPaddingException, NoSuchPaddingException, NoSuchAlgorithmException {
+
+        Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5PADDING");
+        cipher.init(Cipher.ENCRYPT_MODE, key);
+        FileInputStream inputStream = new FileInputStream(inputFile);
+        FileOutputStream outputStream = new FileOutputStream(outputFile);
+        byte[] buffer = new byte[64];
+        int bytesRead;
+        while ((bytesRead = inputStream.read(buffer)) != -1) {
+            byte[] output = cipher.update(buffer, 0, bytesRead);
+            if (output != null) {
+                outputStream.write(output);
+            }
+        }
+        byte[] outputBytes = cipher.doFinal();
+        if (outputBytes != null) {
+            outputStream.write(outputBytes);
+        }
+        inputStream.close();
+        outputStream.close();
+    }
+    public static void decryptFile(SecretKey key,File inputFile, File outputFile) throws InvalidKeyException, IOException, IllegalBlockSizeException, BadPaddingException, NoSuchPaddingException, NoSuchAlgorithmException {
+        System.out.println("Decrypting image "+inputFile.getName());
+        Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5PADDING");
+        cipher.init(Cipher.DECRYPT_MODE, key);
+        FileInputStream inputStream = new FileInputStream(inputFile);
+        FileOutputStream outputStream = new FileOutputStream(outputFile);
+        byte[] buffer = new byte[64];
+        int bytesRead;
+        while ((bytesRead = inputStream.read(buffer)) != -1) {
+            byte[] output = cipher.update(buffer, 0, bytesRead);
+            if (output != null) {
+                outputStream.write(output);
+            }
+        }
+        byte[] outputBytes = cipher.doFinal();
+        if (outputBytes != null) {
+            outputStream.write(outputBytes);
+        }
+        inputStream.close();
+        outputStream.close();
+        System.out.println("Finished Decrypting");
+    }
+    //this function is used to populate the server with encrypted images
+    //Images in meme2 folder are encrypted images of meme folder images
+    //Cute folder images still need to be encrypted
+//    public static void sendFile(SecretKey key){
+//
+//            File f = new File("SecurityProject/Cats/meme");
+//            File[] files = f.listFiles();
+//            if (files != null) {
+//                for (File child : files) {
+//                    try {
+//                        // Do something with child
+//                        // open image
+//                        System.out.println("Sending " + child.getName());
+//                        //BufferedImage img = ImageIO.read(child);
+//                        //ByteArrayOutputStream baos = new ByteArrayOutputStream();
+//                        encryptFile(key, child, new File("SecurityProject/Cats/meme2/"+child.getName()));
+//                        // write image to array stream
+////                    ImageIO.write(img, "jpeg", baos);
+////                    byte[] size = ByteBuffer.allocate(4).putInt(baos.size()).array();
+////
+////                    // write image size, img, and name
+////                    out.write(size);
+////                    out.write(baos.toByteArray());
+////                    out.write(child.getName().getBytes());
+////                    out.flush();
+//
+//                        System.out.println("sent image...\n");
+//                    } catch (IOException e) {
+//                        e.printStackTrace();
+//                    } catch (IllegalBlockSizeException e) {
+//                        e.printStackTrace();
+//                    } catch (NoSuchPaddingException e) {
+//                        e.printStackTrace();
+//                    } catch (BadPaddingException e) {
+//                        e.printStackTrace();
+//                    } catch (NoSuchAlgorithmException e) {
+//                        e.printStackTrace();
+//                    } catch (InvalidKeyException e) {
+//                        e.printStackTrace();
+//                    }
+//                }
+//            }
+//
+//    }
 }
